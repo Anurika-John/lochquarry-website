8a3a5e9b87147da121d2471c8e02e0fadc908c50
Anurika-John <20435391@myclyde.ac.uk>
2026-07-15 18:10:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const typed = new Typed("#typedText", {
+  strings: ["Adventure", "Starts", "Here", "Enjoy"],
+  typeSpeed: 70,
+  backSpeed: 40,
+  backDelay: 1200,
+  loop: true
+});
+
+VanillaTilt.init(document.querySelectorAll(".tilt-card"), {
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.25
+});
+
+
+        
